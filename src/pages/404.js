59170774eaed4c32594d0ff01016d912661e9d78
Alistair Norris <alistairnorris@Orcfull.local>
2021-07-25 20:30:12 +0800

    test push

--- src/pages/404.js
+++ src/pages/404.js
@@ -25,7 +25,7 @@ export default ({ children }) => (
             <p>
               <AlertTriangle size="5rem" />
             </p>
-            <h1>404 - Page Not Found</h1>
+            <h1>404 - Page Not Found!!!!</h1>
             <p>
               We can't find the page you are looking for!
               <br />
